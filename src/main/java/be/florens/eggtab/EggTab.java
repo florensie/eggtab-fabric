91cd012b4b23c8082c36310171e08fe6a125f789
Florens Pauwels <github@florens.be>
2020-07-26 21:37:06 +0200

    Clear enchantments from all ItemGroups

--- src/main/java/be/florens/eggtab/EggTab.java
+++ src/main/java/be/florens/eggtab/EggTab.java
@@ -5,19 +5,16 @@ import be.florens.eggtab.mixin.ItemAccessor;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.Toml4jConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.minecraft.enchantment.EnchantmentTarget;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.SpawnEggItem;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
 
 /**
  * The mod needs to be installed on the server too because it adds an item for the tab icon
@@ -41,30 +38,26 @@ public class EggTab implements ClientModInitializer {
 				() -> new ItemStack(Items.CREEPER_SPAWN_EGG)
 			);
 
-			LOGGER.info("[Egg Tab] Moving spawn eggs");
-
 			// Do all the spawn eggs that have already been registered first
-			for (SpawnEggItem spawnEggItem : SpawnEggItem.getAll()) {
-				changeGroupIfEgg(spawnEggItem);
-			}
-
-			LOGGER.info("[Egg Tab] Now listening for new eggs");
+			LOGGER.info("[Egg Tab] Moving spawn eggs");
+			SpawnEggItem.getAll().forEach(EggTab::changeGroupIfEgg);
 
 			// Make sure we get any eggs that get registered in the future
+			LOGGER.info("[Egg Tab] Now listening for new eggs");
 			RegistryEntryAddedCallback.event(Registry.ITEM).register((rawId, id, item) -> changeGroupIfEgg(item));
 		}
 
 		// Enchanted Books group
 		if (CONFIG.booksGroup) {
 			LOGGER.info("[Egg Tab] Moving enchanted books");
+
+			// Remove enchantments from all groups
+			Arrays.stream(ItemGroup.GROUPS).forEach(ItemGroup::setEnchantments);
+
 			BOOK_GROUP = FabricItemGroupBuilder.build(
 					new Identifier(MOD_ID, "book_group"),
 					() -> new ItemStack(Items.ENCHANTED_BOOK)
 			).setEnchantments(EnchantmentTarget.values()); // Add all enchantments to new group
-
-			// Remove enchantments from Tools and Combat groups
-			ItemGroup.TOOLS.setEnchantments();
-			ItemGroup.COMBAT.setEnchantments();
 		}
 	}
 
